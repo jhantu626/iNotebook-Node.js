e694cf46ee9dcf1f28a68ddceadae8bc1326efd9
jhantu626 <jhantubala626@gmail.com>
2024-05-01 20:35:05 +0530

    Done Create User ModifiedUser and authentication with jwt

---
+++ inotebook-backend/middleware/fetchuser.js
@@ -0,0 +1,22 @@
+const jwt=require('jsonwebtoken')
+require('dotenv').config()
+
+
+const fetchuser=async(req,resp,next)=>{
+   try{
+        const token=req.header('auth-token');
+        if(!token){
+            return resp.json({err: "Enter A valid Token!"}).status(401);
+        }
+        
+        const data=jwt.verify(token,process.env.JWT_SECRET);
+        req.user=data.user;
+        next();
+   }catch(err){
+    console.log(err);
+    resp.json({err: "Internal Server Error!"}).status(500);
+   }
+}
+
+
+module.exports=fetchuser;
